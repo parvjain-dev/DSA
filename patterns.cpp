aea8bea1464ff359a594a983ab94bdb1fd3637b5
Parv Jain <parvjain12@protonmail.com>
2025-10-12 00:27:00 +0530

    pattern12.

--- patterns.cpp
+++ patterns.cpp
@@ -109,6 +109,101 @@ void print8(int n){
     
 }
 
+
+void print9 (int n ){
+    //int i = 0; i < 2*n-1; i++ ?? i = 8 then stars = 2 not 1.
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int stars = i;
+        if (i>n) {
+            stars= 2*n -i;
+        }
+        
+        for (int j = 0; j <stars; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+// void print10(int n ){
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = 0; j <= i; j++)
+//         {
+//             if ((i%2==0 && j%2==0)|| (i%2!=0 && j%2!=0) )
+//             {
+
+//                 cout<<1<<" ";
+//             }else{
+//                 cout<<0<<" ";
+//             }
+            
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+void print10Flip(int n){
+    int start=1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i%2==0)
+        {
+            start=1;
+        }else{
+            start =0;
+        }
+        for (int j = 0; j <=i; j++)
+        {
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+    
+}
+
+//last line is not printing
+// void print11(int n){
+//     for (int i = 0; i < n; i++)
+//     {
+//         for(int j =0; j<i ; j++){
+//             cout<<j+1;
+//         }
+//         for (int k = 0; k< n+2*(1-i); k++)
+//         {
+//             cout<<" ";
+//         }
+//           for(int l =i; l>0 ; l--){
+//             cout<<l;
+//         }
+//         cout<<endl;
+//     }
+    
+// }
+
+void print11(int n){
+    int space = 2*(n-1);
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j =1; j<=i ; j++){
+            cout<<j;
+        }
+        for (int k = 1; k<= space; k++)
+        {
+            cout<<" ";
+        }
+          for(int l =i; l>=1 ; l--){
+            cout<<l;
+        }
+        cout<<endl;
+        space-=2;
+    }
+    
+}
 int main(){
     int n ;
    cout<<"Enter a number: ";
@@ -123,6 +218,10 @@ int main(){
 
 // print6(n);
 
-// print7(n);
-print8(n);
+//  print7(n);
+// print8(n);
+// print9(n);
+// print10Flip(n);
+
+print11(n);
 }
