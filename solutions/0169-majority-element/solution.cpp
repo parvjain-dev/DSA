65f4416b379aadff3ad560e1953afa6e46d94691
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2025-12-30 13:08:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.1 MB (63.62%)

---
+++ solutions/0169-majority-element/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ele= nums[0];
+        int count =1;
+
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i]==ele) count++;
+            else count--;
+            if(count==0) {
+                count =1;
+                ele= nums[i];
+            }
+        }
+        return ele;
+    }
+};
