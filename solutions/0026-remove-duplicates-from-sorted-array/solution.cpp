1a5024e2d1748ec6795d6db5de6030eafb8683de
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-03-31 18:56:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.7 MB (51.55%)

--- solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ solutions/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0;
-        int j =0;
+        int i =0, j= 1;
+
         while(j<nums.size()){
-            if(nums[i]==nums[j]) {
+            if(nums[i]==nums[j]){
                 j++;
             }
             else{
-                swap(nums[i+1],nums[j]);
                 i++;
+                swap(nums[i],nums[j]);
                 j++;
             }
 
         }
-        return i+1;
+        int ans=0; 
+        for(int k=0; k<=i; k++){
+            ans++;
+        }
+        return ans;
+
     }
 };
