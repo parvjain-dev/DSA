883c3961a69f0225aa17113e14f5abf76c757c5d
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-04-26 13:20:20 +0000

    Sync LeetCode submission Runtime - 62 ms (17.49%), Memory - 31.1 MB (20.13%)

--- solutions/0015-3sum/solution.cpp
+++ solutions/0015-3sum/solution.cpp
@@ -2,36 +2,44 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        int j = 1, k = nums.size() - 1;
         sort(nums.begin(), nums.end());
         int i = 0;
-        while (i < nums.size() ) {
-            int j = i + 1, k = nums.size() - 1;
+        while (i <= nums.size() - 3) {
+            j = i + 1, k = nums.size() - 1;
             while (j < k) {
-
-                int summ = nums[i] + nums[j] + nums[k];
-                if (summ > 0) {
-                    k--;
-                } else if (summ < 0) {
-                    j++;
-                } else {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    vector<int> temp= {nums[i], nums[j], nums[k]};
+                    // sort(temp.begin(), temp.end());
                     res.push_back(temp);
+                    k--;
+                    
                     j++;
-
+                    while (k > j && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
                     while (j < k && nums[j] == nums[j - 1]) {
                         j++;
                     }
+                } else if (sum > 0) {
                     k--;
                     while (k > j && nums[k] == nums[k + 1]) {
                         k--;
                     }
+                } else {
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
                 }
             }
             i++;
-            while (i < nums.size() && nums[i] == nums[i - 1]) {
+            while (i <= nums.size() - 3 && nums[i] == nums[i - 1]) {
                 i++;
             }
         }
+
         return res;
     }
 };
