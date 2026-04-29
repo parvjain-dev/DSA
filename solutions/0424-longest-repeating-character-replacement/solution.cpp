f0c48367f49cea8dc456c056b3ab3c8462a80268
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-04-29 06:21:21 +0000

    Sync LeetCode submission Runtime - 1 ms (73.67%), Memory - 10.8 MB (92.46%)

--- solutions/0424-longest-repeating-character-replacement/solution.cpp
+++ solutions/0424-longest-repeating-character-replacement/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    // int max_frequency(vector<int> &v){
-    //     int maxi=INT_MIN;
-    //     for(int i=0; i<v.size(); i++){
-    //         maxi=max(maxi, v[i]);
-    //     }
-    //     return maxi;
-    // }
     int characterReplacement(string s, int k) {
-        vector<int> v(256, 0); 
-        int i=0; 
-        int res=0; 
-        int max_freq=INT_MIN;
-        for(int j=0; j<s.size(); j++){
-            v[s[j]]++;
-            // int max_freq= max_frequency(v);
-            max_freq= max(max_freq, v[s[j]]);
-            while(j-i+1-max_freq>k){
-                v[s[i]]--;
+        int arr[128] = {0};
+
+        // maxfreq-windowsize > k
+        int i = 0;
+        int maxFreq = INT_MIN;
+        int ans = INT_MIN;
+        for (int j = 0; j < s.size(); j++) {
+            arr[s[j]]++;
+            maxFreq = max(maxFreq, arr[s[j]]);
+
+            while ((j - i + 1) - maxFreq > k) {
+                arr[s[i]]--;
                 i++;
             }
-            if(j-i+1-max_freq<=k ){
-                res=max(res, j-i+1);
-            }
+            ans = max(ans, j - i + 1);
         }
-        return res;
+        return ans;
     }
 };
