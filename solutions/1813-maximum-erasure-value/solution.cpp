e32bd10d08472f6d124fd2b439defcb9d8fcb603
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-04-29 17:32:02 +0000

    Sync LeetCode submission Runtime - 161 ms (54.14%), Memory - 118 MB (58.13%)

--- solutions/1813-maximum-erasure-value/solution.cpp
+++ solutions/1813-maximum-erasure-value/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        unordered_map<int,int> mp;
-        int sum=0, maxi=0, i=0;
+        unordered_map<int, int> freq;
+        int sum = 0, maxi = INT_MIN;
 
-        for(int j=0; j<nums.size();j++){
-            mp[nums[j]]++;
-            sum+=nums[j];
-            while(mp[nums[j]]>1){
-                sum-=nums[i];
-                mp[nums[i]]--;
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            sum += nums[j];
+            freq[nums[j]]++;
+            while (freq.size() != j - i + 1) {
+                sum -= nums[i];
+                freq[nums[i]]--;
+                if (freq[nums[i]] == 0)
+                    freq.erase(nums[i]);
                 i++;
-                
             }
-            maxi=max(sum,maxi);
-
+            maxi = max(sum, maxi);
         }
         return maxi;
     }
