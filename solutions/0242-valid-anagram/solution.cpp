7ff4e3cbdf1cf9366c97ab0bd4b5fdb5fafbce7b
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-01 17:23:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (10.36%)

---
+++ solutions/0242-valid-anagram/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> mp;
+        if(s.size()!=t.size()) return false;
+        for (int i = 0; i < s.size(); i++) {
+            mp[s[i]]++;
+        }
+        for (int j = 0; j < t.size(); j++) {
+
+            mp[t[j]]--;
+            if (mp[t[j]] < 0) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+};
