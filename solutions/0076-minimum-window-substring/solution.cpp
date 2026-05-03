f1651ca27cd8dd0c5851bd921e31c5fd0f7daccd
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-03 15:08:03 +0000

    Sync LeetCode submission Runtime - 16 ms (27.62%), Memory - 11.7 MB (13.97%)

--- solutions/0076-minimum-window-substring/solution.cpp
+++ solutions/0076-minimum-window-substring/solution.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> freq;
-        unordered_map<char, int> window;
+        unordered_map<char, int> need;
+        for (int k = 0; k < t.size(); k++) {
+            need[t[k]]++;
+        }
+
+        int required = need.size();
+
+        int formed = 0;
+        int len = INT_MAX;
         string res = "";
         int i = 0;
-        int len = INT_MAX;
-        for (int k = 0; k < t.size(); k++) {
-            freq[t[k]]++;
-        }
-        int required = freq.size();
-        int formed = 0;
-        int start =0;
+        int start;
+        unordered_map<char, int> freq;
+
         for (int j = 0; j < s.size(); j++) {
-            window[s[j]]++;
-            if (freq.count(s[j]) && window[s[j]] == freq[s[j]] ) {
+            freq[s[j]]++;
+            if (need.count(s[j]) && freq[s[j]] == need[s[j]]) {
                 formed++;
             }
-
             while (formed == required) {
                 if (len > j - i + 1) {
-                    len= j-i+1;
+                    len = j - i + 1;
                     start =i;
                 }
-                window[s[i]]--;
-                if (freq.count(s[i]) && window[s[i]] < freq[s[i]]) {
+                freq[s[i]]--;
+                if(freq[s[i]]==0) freq.erase(s[i]);
+                if (need.count(s[i]) && freq[s[i]] < need[s[i]]) {
                     formed--;
                 }
+                
                 i++;
+                
             }
+         
         }
-        res= s.substr(start, len);
-        if(len == INT_MAX){
-            return "";
-        }else{
-            return res;
-        }
+        if(len==INT_MAX) return "";
+        res=s.substr(start, len);
+        return res;
     }
 };
