bd94117301cc23c382a84e23b2c91f1a4b3c1f83
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-03 14:24:49 +0000

    Sync LeetCode submission Runtime - 176 ms (8.39%), Memory - 17.8 MB (58.79%)

--- solutions/0152-maximum-product-subarray/solution.cpp
+++ solutions/0152-maximum-product-subarray/solution.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int pos = 0, neg = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0)
-                pos++;
-            else if (nums[i] < 0)
-                neg++;
-        }
-        int ans = nums[0];
-        int maxi = INT_MIN;
-        int prefix = 1, suffix = 1;
-        if (nums.size() < 2)
-            return nums[0];
-        for(int i=0; i< nums.size(); i++){
-            if(prefix ==0) prefix=1;
-            if(suffix==0) suffix=1;
-            prefix*= nums[i];
-            suffix*= nums[nums.size()-i-1];
+        int maxEnding = nums[0];
+        int minEnding=nums[0];
+        int ans=nums[0];
+        int j=1;
+        int temp, temp2 ;
+        while(j<nums.size()){
+            int v1 = maxEnding*nums[j];
+            int v2= minEnding* nums[j];
 
-            ans= max(ans,max(prefix,suffix));
+            int v3 = nums[j];
+            maxEnding= max(v3, max(v1,v2));
+            minEnding= min(v3, min(v1,v2));
+            cout<<maxEnding<<" "<<minEnding<<" "<<endl;
+            temp= max(maxEnding, minEnding);
+            // temp2= max(temp, nums[j]);
+            ans=max(ans, temp);
+            j++;
+            
+            
         }
-
         return ans;
     }
 };
