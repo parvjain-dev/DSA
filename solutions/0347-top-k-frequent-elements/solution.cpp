8303c876bd4d62c35316151cf73271dae670b914
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-04 19:29:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.9 MB (16.89%)

--- solutions/0347-top-k-frequent-elements/solution.cpp
+++ solutions/0347-top-k-frequent-elements/solution.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> mp;
-        vector<int> res;
-        int temp=k;
-        for(int i =0; i< nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             mp[nums[i]]++;
         }
+        vector<vector<int>> bucket(nums.size() + 1);
 
-        vector<vector<int>> bucket(nums.size()+1);
-        for(auto it: mp){
+        for (auto it : mp) {
             bucket[it.second].push_back(it.first);
         }
-        for(int i =bucket.size()-1; i>= 0; i--){
-            for(int j=bucket[i].size()-1; j>=0; j--){
+        vector<int> res;
+        int temp = k;
+        for (int i = bucket.size()-1; i >= 1; i--) {
+            for (int j = 0; j <bucket[i].size(); j++) {
                 res.push_back(bucket[i][j]);
                 temp--;
-                if(temp==0) break;
+                if (temp == 0) {
+                    break;
+                }
             }
-            if(temp==0) break;
-            
+            if (temp == 0) {
+                break;
+            }
         }
         return res;
     }
