e080ed5c707278a083882d556bc308295152be05
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-06 19:26:01 +0000

    Sync LeetCode submission Runtime - 22 ms (32.44%), Memory - 26.1 MB (47.52%)

--- solutions/0049-group-anagrams/solution.cpp
+++ solutions/0049-group-anagrams/solution.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
-        for (int i = 0; i < strs.size(); i++) {
-            string temp = strs[i];
+
+        for(int i=0;i<strs.size();i++){
+            string temp= strs[i];
             sort(temp.begin(), temp.end());
             mp[temp].push_back(strs[i]);
         }
-        
 
-        vector<vector<string>> res;
-        int index=0;
-        for(auto it :mp){
-            res.push_back(it.second);
+        vector<vector<string>> bucket;
+        for(auto it:mp){
+            bucket.push_back(it.second);
         }
-        return res;
+        return bucket;
     }
 };
