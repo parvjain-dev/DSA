b8a61480c886b6030a230ee9214e1173718f6ce0
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-09 18:07:09 +0000

    Sync LeetCode submission Runtime - 3 ms (78.96%), Memory - 11.6 MB (35.78%)

--- solutions/0438-find-all-anagrams-in-a-string/solution.cpp
+++ solutions/0438-find-all-anagrams-in-a-string/solution.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> res;
-        int formed = 0;
-        unordered_map<char, int> need;
-        unordered_map<char, int> freq;
         int k = p.size();
+
+        unordered_map<int, int> need;
+        unordered_map<int, int> freq;
+        vector<int> res;
         for (int i = 0; i < k; i++) {
             need[p[i]]++;
         }
         int required = need.size();
-
+        int formed = 0;
         for (int i = 0; i < k; i++) {
-            freq[s[i]]++;
-            if (freq[s[i]] == need[s[i]]) {
-                formed++;
-            }
-        }
-        if (formed == required) {
-            res.push_back(0);
-        }
-
-        for (int i = k; i < s.size(); i++) {
-            if (need.count(s[i-k]) && freq[s[i - k]] == need[s[i - k]]) {
-                formed--;
-            }
-            freq[s[i - k]]--;
-            if (freq[s[i - k]] == 0)
-                freq.erase(s[i - k]);
             freq[s[i]]++;
             if (need.count(s[i]) && freq[s[i]] == need[s[i]]) {
                 formed++;
             }
-            if (formed == required) {
-                res.push_back(i - k + 1);
+        }
+        if (required == formed)
+            res.push_back(0);
+
+        for (int i = k; i < s.size(); i++) {
+            
+            if (need.count(s[i-k]) &&freq[s[i - k]] == need[s[i - k]]) {
+                formed--;
             }
+            freq[s[i - k]]--;
+            freq[s[i]]++;
+            if (need.count(s[i]) && freq[s[i]] == need[s[i]]) {
+                formed++;
+            }
+            if (required == formed)
+                res.push_back(i - k + 1);
         }
         return res;
     }
