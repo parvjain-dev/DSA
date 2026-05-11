a1d0289dc98169b6711332005c14e7aa64129064
Parv Jain <83375298+parvjain-dev@users.noreply.github.com>
2026-05-11 17:04:55 +0000

    Sync LeetCode submission Runtime - 4 ms (43.06%), Memory - 22.6 MB (63.54%)

--- solutions/0036-valid-sudoku/solution.cpp
+++ solutions/0036-valid-sudoku/solution.cpp
@@ -1,62 +1,66 @@
 class Solution {
 public:
     bool rowValidation(vector<vector<char>>& board) {
+        unordered_set<char> st;
 
         for (int i = 0; i < board.size(); i++) {
-            vector<int> freq(10, 0);
             for (int j = 0; j < board[i].size(); j++) {
-                if (board[i][j] == '.') {
+                if (board[i][j] == '.')
                     continue;
-                } else if (freq[board[i][j] - '0'] == 1) {
-                    return false;
+                else if (!st.count(board[i][j])) {
+                    st.insert(board[i][j]);
                 } else {
-                    freq[board[i][j] - '0']++;
+                    return false;
                 }
             }
+            st.clear();
         }
         return true;
     }
 
     bool colValidation(vector<vector<char>>& board) {
+        unordered_set<char> st;
 
         for (int i = 0; i < board.size(); i++) {
-            vector<int> freq(10, 0);
             for (int j = 0; j < board[i].size(); j++) {
-                if (board[j][i] == '.') {
+                if (board[j][i] == '.')
                     continue;
-                } else if (freq[board[j][i] - '0'] == 1) {
-                    return false;
+                else if (!st.count(board[j][i])) {
+                    st.insert(board[j][i]);
                 } else {
-                    freq[board[j][i] - '0']++;
+                    return false;
                 }
             }
+            st.clear();
         }
         return true;
     }
     bool boxValidation(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i = i + 3) {
-            for (int j = 0; j < 9; j = j + 3) {
-                vector<int> freq(10, 0);
-                for (int k = i; k < i + 3; k++) {
-                    for (int l = j; l < j + 3; l++) {
-                        if (board[k][l] == '.') {
+        unordered_set<char> st;
+
+        for (int i = 0; i < board.size(); i = i + 3) {
+            for (int j = 0; j < board[i].size(); j = j + 3) {
+                for (int k = i; k < i+3; k++) {
+                    for (int l = j; l < j+3; l++) {
+                        if (board[k][l] == '.')
                             continue;
-                        } else if (freq[board[k][l] - '0'] == 1) {
-                            return false;
+                        else if (!st.count(board[k][l])) {
+                            st.insert(board[k][l]);
                         } else {
-                            freq[board[k][l] - '0']++;
+                            return false;
                         }
                     }
                 }
+                st.clear();
             }
         }
         return true;
     }
     bool isValidSudoku(vector<vector<char>>& board) {
-
-        if(rowValidation(board)&&colValidation(board) && boxValidation(board)){
+        if (rowValidation(board) && colValidation(board) &&
+            boxValidation(board))
             return true;
-        }
+
         return false;
     }
 };
